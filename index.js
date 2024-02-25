96a329ba97351069d07705e7acfae94b3b8b842f
madiinemm <uraimovamadina179@gmail.com>
2024-02-25 22:59:28 +0500

    Vazifa bajarldi

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const from = document.getElementById('from');
+const to = document.getElementById('to');
+const amount = document.getElementById('amount');
+const currencyFrom = document.getElementById('currency-from');
+const currencyTo = document.getElementById('currency-to');
+const convertBtn = document.getElementById('convert-btn');
+
+
